e556a513878ba72c7d476d134c20d2db8512c4cf
Korre Henry <55932637+HackMasterJohn@users.noreply.github.com>
2023-04-01 04:10:36 -0700

    Add more changes

---
+++ contacts-manager-app/src/database/firestore.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSNO5aYUyJPi6CXiQ6oTLa8o8fEGzFxCA",
+  authDomain: "contactproject-67931.firebaseapp.com",
+  projectId: "contactproject-67931",
+  storageBucket: "contactproject-67931.appspot.com",
+  messagingSenderId: "557651510791",
+  appId: "1:557651510791:web:cbda90865828e61d94cca2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
