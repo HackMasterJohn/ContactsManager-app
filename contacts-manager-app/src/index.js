3d2d800957795a5bcd59350fdc22f701f7fd554a
Korre Henry <55932637+HackMasterJohn@users.noreply.github.com>
2023-04-09 21:44:25 -0700

    Adds more changes

--- contacts-manager-app/src/index.js
+++ contacts-manager-app/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import { App } from './components/App.js';
+import { SaveContact } from './pages/SaveContact.js';
 import { SideBarMenu } from './components/SideBarMenu.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SideBarMenu />
+    <SaveContact />
   </React.StrictMode>
 );
 
---
+++ contacts-manager-app/src/pages/SaveContact.js
@@ -0,0 +1,46 @@
+import '../styles/App.scss';
+
+export function SaveContact() {
+  return (
+    <div className="addContact">
+        <div class="center">
+            <h1>Add New Contact</h1>
+            <form method="post">
+                <div class="txt_field">
+                    <input type="text" required/> 
+                    <span></span>
+                    <label>First Name</label>
+                </div>
+
+                <div class="txt_field">
+                    <input type="text" required/> 
+                    <span></span>
+                    <label>Last Name</label>
+                </div>
+            
+                <div class="txt_field">
+                    <input type="text" required/> 
+                    <span></span>
+                    <label>Address</label>
+                </div>
+
+                <div class="txt_field">
+                    <input type="text" required/> 
+                    <span></span>
+                    <label>Cell Number</label>
+                </div>
+
+                <div class="txt_field">
+                    <input type="text" required/> 
+                    <span></span>
+                    <label>Home Number</label>
+                </div>
+
+                <input type="submit" value="Save Contact"/>
+                <div class="signup_link"><a href="#">Cancel</a>
+                </div>
+            </form>
+        </div>
+    </div>
+    );
+}
